38119aed9bc5a91af346e660e26533fab3cf97a4
Braintech22 <datveryjimmy@gmail.com>
2022-06-17 20:34:11 -0700

    new commit

---
+++ opencv.py
@@ -0,0 +1,12 @@
+import cv2
+print(cv2. __version__)
+cam=cv2.VideoCapture(0)
+while True:
+    ignore, frame = cam.read()
+    greyFrame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('my webcam',greyFrame)
+    cv2.moveWindow('my webcam', 0,0)
+    if cv2.waitKey(1) & 0xff ==ord('q'):
+        break
+cam.release()
+
